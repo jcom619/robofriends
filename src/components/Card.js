2036cc3beed69c65e8c45f253184f0db92fac7bd
jcom619 <46643961+jcom619@users.noreply.github.com>
2019-10-28 20:43:13 -0700

    more tests

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
 const Card = ({ name, email, id }) => {
-
   return (
-    <div className='bg-lightest-blue br3 pa3 ma2 dib f1 tc grow bw2 shadow-5'>
+    <div className='bg-lightest-blue br3 pa3 ma2 dib f1  grow bw2 shadow-5'>
       <img src={`https://robohash.org/${ id }?size=200x200`} alt='HiMundo!'/>
       <div>
         <h2>{ name }</h2>
